bc913c0357f0f7ed0b5cb075808e7c43b7a9b3b8
Anton Guletskiy <anton.guletskiy@gmail.com>
2024-02-26 19:20:22 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import CommentsList from './CommentList';
 import Message from './Message';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
        <h1>My Message</h1>
        <Message text="Hello, world!"/>
+       <CommentsList/>
       </header>
     </div>
   );
